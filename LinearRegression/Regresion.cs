4704b1999108e8a45e1f66094218c52db0ce5a24
Jill England <jill.england@comcast.net>
2025-06-02 22:41:41 -0700

    comment update

--- LinearRegression/Regresion.cs
+++ LinearRegression/Regresion.cs
@@ -12,20 +12,20 @@
 ///     I can return the following properties of that data
 ///     Two dimensional statistical data
 ///     sy  = sum y
-///     sy2 = sum y**2
+///     sy2 = sum y^2
 ///     n
 ///     sx  = sum x
 ///     sx2 = sum x**2
 ///     sxy = sum x * y
 ///     mean x = mx = sx/n
 ///     mean y = my = sy/n
-///     standard deviation x = qx = sqr((sx2 - (sx**2 /n)) / (n-1) )
-///     standard deviation y = qy = sqr((sy2 - (sy**2 /n)) / (n-1) )
-///     variance x = qx2 = sx2 / n - mx**2
-///     variance y = qy2 = sy2 / n - my**2
+///     standard deviation x = qx = sqr((sx2 - (sx^2 /n)) / (n-1) )
+///     standard deviation y = qy = sqr((sy2 - (sy^2 /n)) / (n-1) )
+///     variance x = qx2 = sx2 / n - mx^2
+///     variance y = qy2 = sy2 / n - my^2
 ///     Use N weighting for population studies
 ///     and N-1 for sample studies
-///     Slope = m = sxy - (sx*sy)/n / sx2 - sx**2 /n
+///     Slope = m = sxy - (sx*sy)/n / sx2 - sx^2 /n
 ///     yIntercept = b = (sy - m*sx) / n
 ///     correlation coefficient = R = (m qx) /qy
 /// </remarks>
