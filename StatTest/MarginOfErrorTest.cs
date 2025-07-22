b13cc39c100d3b968a0a9b6b63fe533338115b25
Jill England <jill.england@comcast.net>
2025-07-21 21:03:37 -0700

    Rename Test Class

---
+++ StatTest/MarginOfErrorTest.cs
@@ -0,0 +1,53 @@
+﻿using LinearRegression;
+using Xunit.Abstractions;
+
+namespace RegressionTest;
+
+public class MarginOfErrorTest(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void MeanConfidenceWithVerify()
+    {
+        // Arrange
+        // Arrange
+        var dataPoints = new List<(double x, double y)>
+        {
+            (1.0, 2.0),
+            (2.0, 3.0),
+            (3.0, 5.0),
+            (4.0, 4.0),
+            (5.0, 6.0)
+        };
+
+        var regression = new RegressionPvalue(dataPoints);
+
+        // Act
+        var moe = regression.MarginOfError(false, 0.95);
+
+        // Assert
+        Assert.Equal(regression.MeanX(), moe.Mean);
+
+        testOutputHelper.WriteLine($"Mean and MarginOfError: [{moe.Mean}, {moe.MarginOfError:F4}]");
+    }
+
+    [Fact]
+    public void MeanConfidenceWithoutVerify()
+    {
+        // Arrange
+        var dataPoints = new List<(double x, double y)>
+        {
+            (1.0, 2.0),
+            (2.0, 3.0),
+            (3.0, 5.0),
+            (4.0, 4.0),
+            (5.0, 6.0)
+        };
+        var regression = new RegressionPvalue(dataPoints);
+        // Act
+        var moe = regression.MarginOfError(true, 0.95);
+
+        // Assert
+        Assert.Equal(regression.MeanY(), moe.Mean);
+        testOutputHelper.WriteLine($"Mean and MarginOfError: [{moe.Mean}, {moe.MarginOfError:F4}]");
+    }
+}
